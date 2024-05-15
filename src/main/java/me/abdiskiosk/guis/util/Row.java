407d8077b97803d8d4629cc101d3e6311c8abbdc
AbdisKiosk <85417512+AbdisKiosk@users.noreply.github.com>
2024-05-15 16:37:57 +0200

    add row

---
+++ src/main/java/me/abdiskiosk/guis/util/Row.java
@@ -0,0 +1,15 @@
+package me.abdiskiosk.guis.util;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Row {
+
+    public static final Collection<Integer> FIRST = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8);
+    public static final Collection<Integer> SECOND = Arrays.asList(9, 10, 11, 12, 13, 14, 15, 16, 17);
+    public static final Collection<Integer> THIRD = Arrays.asList(18, 19, 20, 21, 22, 23, 24, 25, 26);
+    public static final Collection<Integer> FOURTH = Arrays.asList(27, 28, 29, 30, 31, 32, 33, 34, 35);
+    public static final Collection<Integer> FIFTH = Arrays.asList(36, 37, 38, 39, 40, 41, 42, 43, 44);
+    public static final Collection<Integer> SIXTH = Arrays.asList(45, 46, 47, 48, 49, 50, 51, 52, 53);
+
+}
