977bd236a09d9d294654b9a15c418e9a01c03bf8
AbdisKiosk <85417512+AbdisKiosk@users.noreply.github.com>
2024-05-17 15:18:13 +0200

    poggers

--- src/main/java/me/abdiskiosk/guis/pagination/PaginationHandler.java
+++ src/main/java/me/abdiskiosk/guis/pagination/PaginationHandler.java
@@ -43,30 +43,23 @@ public class PaginationHandler<T> {
         this.itemConverter = itemConverter;
         this.slots = slots;
         this.nullItem = nullItem;
-
-        setPage(1, false);
     }
 
-    public void setPageSafe(int page) {
-        setPage(page, true);
+    public boolean setPageIfNotEmpty(int page) {
+        List<T> objects = objectProvider.get(getRangeMin(page), getRangeMax(page));
+        if(objects.isEmpty()) {
+            return false;
+        }
+        setPage(page);
+        return true;
     }
 
     public void setPage(int page) {
-        setPage(page, false);
+        setObjects(objectProvider.get(getRangeMin(page), getRangeMax(page)));
     }
 
-    public void setPage(int page, boolean safe) {
+    protected void setObjects(@NotNull List<T> objects) {
         clearSlots();
-        if(page < 1) {
-            return;
-        }
-
-        List<T> objects = objectProvider.get(getRangeMin(page), getRangeMax(page));
-
-        if(objects.isEmpty() && safe) {
-            return;
-        }
-
         for(int i = 0; i < slots.size(); i++) {
             int slot = slots.get(i);
             if(i < objects.size()) {
---
+++ src/main/java/me/abdiskiosk/guis/state/objects/PageState.java
@@ -0,0 +1,55 @@
+package me.abdiskiosk.guis.state.objects;
+
+import me.abdiskiosk.guis.pagination.PaginationHandler;
+import me.abdiskiosk.guis.state.State;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class PageState implements State<Integer> {
+
+    private final IntState state;
+    private final PaginationHandler<?> paginationHandler;
+
+    public PageState(PaginationHandler<?> paginationHandler) {
+        this.state = new IntState(1);
+        this.paginationHandler = paginationHandler;
+        set(1);
+    }
+
+    @Override
+    public Integer get() {
+        return state.get();
+    }
+
+    @Override
+    public void set(Integer value) {
+        if(value < 0) {
+            return;
+        }
+        if(paginationHandler.setPageIfNotEmpty(value)) {
+            state.set(value);
+        }
+    }
+
+    @Override
+    public void update() {
+        state.update();
+    }
+
+    @Override
+    public void subscribe(@NotNull Consumer<Integer> subscriber) {
+        state.subscribe(subscriber);
+    }
+
+    @Override
+    public void subscribe(@NotNull BiConsumer<Integer, Integer> subscriber) {
+        state.subscribe(subscriber);
+    }
+
+    @Override
+    public void unsubscribe(@NotNull Consumer<Integer> subscriber) {
+        state.unsubscribe(subscriber);
+    }
+}
