61fad0cbf67cb97bee4c0006e22a483560216bae
AbdisKiosk <85417512+AbdisKiosk@users.noreply.github.com>
2024-05-19 00:30:31 +0200

    fasdfas

--- src/main/java/me/abdiskiosk/guis/util/Placeholders.java
+++ src/main/java/me/abdiskiosk/guis/util/Placeholders.java
@@ -13,42 +13,42 @@ public class Placeholders {
         return Arrays.asList(states);
     }
 
-    public static Collection<StaticNamedState<?>> of(@NotNull Object o1, @NotNull String name1) {
+    public static Collection<StaticNamedState<?>> of(@NotNull String name1, @NotNull Object o1) {
         return of(State.immutable(o1, name1));
     }
 
-    public static Collection<StaticNamedState<?>> of(@NotNull Object o1, @NotNull String name1,
-                                                     @NotNull Object o2, @NotNull String name2) {
+    public static Collection<StaticNamedState<?>> of(@NotNull String name1, @NotNull Object o1,
+                                                     @NotNull String name2, @NotNull Object o2) {
         return of(State.immutable(o1, name1), State.immutable(o2, name2));
     }
 
-    public static Collection<StaticNamedState<?>> of(@NotNull Object o1, @NotNull String name1,
-                                                     @NotNull Object o2, @NotNull String name2,
-                                                     @NotNull Object o3, @NotNull String name3) {
+    public static Collection<StaticNamedState<?>> of(@NotNull String name1, @NotNull Object o1,
+                                                     @NotNull String name2, @NotNull Object o2,
+                                                     @NotNull String name3, @NotNull Object o3) {
         return of(State.immutable(o1, name1), State.immutable(o2, name2), State.immutable(o3, name3));
     }
 
-    public static Collection<StaticNamedState<?>> of(@NotNull Object o1, @NotNull String name1,
-                                                     @NotNull Object o2, @NotNull String name2,
-                                                     @NotNull Object o3, @NotNull String name3,
-                                                     @NotNull Object o4, @NotNull String name4) {
+    public static Collection<StaticNamedState<?>> of(@NotNull String name1, @NotNull Object o1,
+                                                     @NotNull String name2, @NotNull Object o2,
+                                                     @NotNull String name3, @NotNull Object o3,
+                                                     @NotNull String name4, @NotNull Object o4) {
         return of(State.immutable(o1, name1), State.immutable(o2, name2), State.immutable(o3, name3), State.immutable(o4, name4));
     }
 
-    public static Collection<StaticNamedState<?>> of(@NotNull Object o1, @NotNull String name1,
-                                                     @NotNull Object o2, @NotNull String name2,
-                                                     @NotNull Object o3, @NotNull String name3,
-                                                     @NotNull Object o4, @NotNull String name4,
-                                                     @NotNull Object o5, @NotNull String name5) {
+    public static Collection<StaticNamedState<?>> of(@NotNull String name1, @NotNull Object o1,
+                                                     @NotNull String name2, @NotNull Object o2,
+                                                     @NotNull String name3, @NotNull Object o3,
+                                                     @NotNull String name4, @NotNull Object o4,
+                                                     @NotNull String name5, @NotNull Object o5) {
         return of(State.immutable(o1, name1), State.immutable(o2, name2), State.immutable(o3, name3), State.immutable(o4, name4), State.immutable(o5, name5));
     }
 
-    public static Collection<StaticNamedState<?>> of(@NotNull Object o1, @NotNull String name1,
-                                                     @NotNull Object o2, @NotNull String name2,
-                                                     @NotNull Object o3, @NotNull String name3,
-                                                     @NotNull Object o4, @NotNull String name4,
-                                                     @NotNull Object o5, @NotNull String name5,
-                                                     @NotNull Object o6, @NotNull String name6) {
+    public static Collection<StaticNamedState<?>> of(@NotNull String name1, @NotNull Object o1,
+                                                     @NotNull String name2, @NotNull Object o2,
+                                                     @NotNull String name3, @NotNull Object o3,
+                                                     @NotNull String name4, @NotNull Object o4,
+                                                     @NotNull String name5, @NotNull Object o5,
+                                                     @NotNull String name6, @NotNull Object o6) {
         return of(State.immutable(o1, name1), State.immutable(o2, name2), State.immutable(o3, name3), State.immutable(o4, name4), State.immutable(o5, name5), State.immutable(o6, name6));
     }
 }
