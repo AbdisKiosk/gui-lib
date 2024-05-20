e309f800e65dffa9156d8b15d04f656cc0a35b18
AbdisKiosk <85417512+AbdisKiosk@users.noreply.github.com>
2024-05-20 23:00:18 +0200

    scheudler async

--- src/main/java/me/abdiskiosk/guis/util/Scheduler.java
+++ src/main/java/me/abdiskiosk/guis/util/Scheduler.java
@@ -2,34 +2,33 @@ package me.abdiskiosk.guis.util;
 
 import me.abdiskiosk.guis.GUIManager;
 import me.abdiskiosk.guis.gui.GUI;
-import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
 public class Scheduler {
 
     public static void whileOpen(@NotNull GUI gui, @NotNull Runnable runnable, int waitTicks) {
-        new Task(gui, runnable).runTaskTimer(GUIManager.getPlugin(), 0, waitTicks);
+        new Task(gui, waitTicks, runnable).runTask(GUIManager.getPlugin());
     }
 
     public static class Task extends BukkitRunnable {
 
         private final Runnable run;
+        private final int waitTicks;
         private final GUI gui;
 
-        public Task(@NotNull GUI gui, @NotNull Runnable run) {
+        public Task(@NotNull GUI gui, int waitTicks, @NotNull Runnable run) {
             this.gui = gui;
             this.run = run;
+            this.waitTicks = waitTicks;
         }
 
         @Override
         public void run() {
-            if(!gui.isOpen()) {
-                cancel();
-                return;
-            }
             run.run();
+            if(gui.isOpen()) {
+                runTaskLater(GUIManager.getPlugin(), waitTicks);
+            }
         }
-
     }
 }
